fefb2fbd7f97ec672deb7fef2c2ce2c4962896f5
yanhao <liyanhao312@gmail.com>
2019-07-31 10:33:26 +0200

    update nginx Dockerfile

--- install.sh
+++ install.sh
@@ -4,17 +4,23 @@
 
 cd frontend-backend-docker/
 git clone git@git.gpac-licensing.com:yanhao/janus-webrtc-backend.git
+git pull
 cd janus-webrtc-backend/ 
 git checkout dev 
 cd ..
 git clone git@git.gpac-licensing.com:yanhao/janus-webrtc-frontend.git
+git pull
 cd .. 
 git clone git@git.gpac-licensing.com:yanhao/janus-webrtc-gateway-docker.git
+git pull
 cd janus-webrtc-gateway-docker/
 git checkout dev
 
 echo "All projects have been downloaded."
-echo "To build the demo: 
-        docker-compose build
-To run the demo:
-        docker-compose up"
+echo "To build the docker images: 
+        sudo docker-compose build
+To run the docker images:
+        sudo docker-compose up
+Then open a chrome browser and go to
+	http://localhost:9999
+Click \"Start\" to test"
